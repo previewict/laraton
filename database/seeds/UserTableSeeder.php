b36bba7b66f2a55a93780b9ebf8fb615272c0656
Imtiaz Pabel <imtiazpabel@yahoo.com>
2015-10-07 13:09:38 +0600

    seeder user model namespace changed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,7 +11,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create(array(
+        factory(\laraton\User::class)->create(array(
             'name' => 'previewICT',
             'email' => 'info@miteksoftware.com',
             'password' => bcrypt('ltqpsmr7')
